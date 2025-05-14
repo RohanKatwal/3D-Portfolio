0cfe196deeb3fca5fe214cd5cd35252bd4c612ea
RohanKatwal <rohanwork909@gmail.com>
2025-05-14 15:22:07 +0545

    added room wiht light init

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -2,6 +2,7 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import React from 'react'
 import { useMediaQuery } from 'react-responsive';
+import { Room } from './room';
 
 const HeroExperience = () => {
   
@@ -10,7 +11,7 @@ const HeroExperience = () => {
   return (
     <Canvas camera={{position:[0,0,15], fov:45}}>
         <ambientLight intensity={0.2} color="#1a1a40"/>
-        <directionalLight position={[5,5,5]} intensity={5}/>
+        <directionalLight position={[5,5,5]} intensity={2}/>
 
         <OrbitControls
           enablePan={false}
@@ -20,10 +21,20 @@ const HeroExperience = () => {
           minPolarAngle={Math.PI/5}
           maxPolarAngle={Math.PI/2}
         />
-        <mesh>
+        {/* <mesh>
             <boxGeometry args={[1,1,1]}/>
             <meshStandardMaterial color="blue"/>
-        </mesh>
+        </mesh> */}
+        <group
+          scale={isMobile?0.7 :1}
+          position={[0,-3.5,0]}
+          rotation={[0,-Math.PI/4,0]}
+          
+          >
+
+          <Room/>
+
+        </group>
     </Canvas>
   )
 }
