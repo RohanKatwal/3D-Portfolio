90d7eba88e08b0a19c1cf1b21d4b4ace19464386
RohanKatwal <rohanwork909@gmail.com>
2025-05-14 15:56:23 +0545

    3d room completed

--- src/components/HeroModels/HeroExperience.jsx
+++ src/components/HeroModels/HeroExperience.jsx
@@ -3,6 +3,7 @@ import { OrbitControls } from '@react-three/drei'
 import React from 'react'
 import { useMediaQuery } from 'react-responsive';
 import { Room } from './room';
+import HeroLights from './HeroLights';
 
 const HeroExperience = () => {
   
@@ -10,9 +11,7 @@ const HeroExperience = () => {
   const isMobile=useMediaQuery({query:'(max-width:768px'});
   return (
     <Canvas camera={{position:[0,0,15], fov:45}}>
-        <ambientLight intensity={0.2} color="#1a1a40"/>
-        <directionalLight position={[5,5,5]} intensity={2}/>
-
+        <HeroLights/>
         <OrbitControls
           enablePan={false}
           enableZoom={!isTablet}
