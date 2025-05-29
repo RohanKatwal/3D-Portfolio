8c53fdac709f479ed71e3afe661492859f7de940
Rohan Katwal <rohanwork909@gmail.com>
2025-05-29 14:04:06 +0545

    animation using gsap in cards

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -3,6 +3,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
+gsap.registerPlugin(ScrollTrigger);
 const ShowcaseSection = () => {
     const sectionRef = useRef(null);
     const pathaoRef = useRef(null);
