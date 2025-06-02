d41de344a2b5c43e13421689a5953380a17633b3
Rohan Katwal <rohanwork909@gmail.com>
2025-06-02 14:29:46 +0545

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/Navbar"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
 import ShowcaseSection2 from "./sections/ShowcaseSection2"
@@ -5,6 +6,7 @@ import ShowcaseSection2 from "./sections/ShowcaseSection2"
 const App = () => {
   return (
     <>
+    <Navbar/>
     <Hero/>
     <ShowcaseSection/>
     <ShowcaseSection2/>
