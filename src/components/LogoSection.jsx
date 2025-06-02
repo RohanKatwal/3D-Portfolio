b9a04492914f4147f2ea3f4306dc3dcf563700b2
Rohan Katwal <rohanwork909@gmail.com>
2025-06-02 16:39:21 +0545

    logo section

--- src/components/LogoSection.jsx
+++ src/components/LogoSection.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import { logoIconsList } from '../constants'
+
+const LogoIcon=({icon})=>{
+    return (
+        <div className='flex-none flex-center marquee-item'>
+            <img src={icon.imgPath} alt={icon.name} />
+        </div>
+    )
+}
 
 const LogoSection = () => {
   return (
@@ -8,7 +17,12 @@ const LogoSection = () => {
 
         <div className="marquee h-52">
             <div className="marquee-box md-gap-12 gap-5">
-
+                {logoIconsList.map((icon)=>(
+                    <LogoIcon key={icon.name} icon={icon}/>
+                ))}
+                {logoIconsList.map((icon)=>(
+                    <LogoIcon key={icon.name} icon={icon}/>
+                ))}
             </div>
         </div>
     </div>
