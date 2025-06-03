2b2b354cd3c15c587ecc74d6008145b93711cde9
Rohan Katwal <rohanwork909@gmail.com>
2025-06-03 13:18:41 +0545

    delete showcasesection2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import LogoSection from "./components/LogoSection"
 import Navbar from "./components/Navbar"
 import Hero from "./sections/Hero"
 import ShowcaseSection from "./sections/ShowcaseSection"
-import ShowcaseSection2 from "./sections/ShowcaseSection2"
 
 const App = () => {
   return (
